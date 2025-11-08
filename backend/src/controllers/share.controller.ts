b1ec87d6c1b9266882df3946af42ba3c9ef69b03
Ayush Thakur <ayushh1335@proton.me>
2025-11-08 21:47:56 +0530

    fix: getShareableLink

--- backend/src/controllers/share.controller.ts
+++ backend/src/controllers/share.controller.ts
@@ -1,4 +1,6 @@
+import contentModel from "../model/content.model.js";
 import linkModel from "../model/link.model.js";
+import userModel from "../model/user.model.js";
 import generateRandomHash from "../utils.js";
 import type { Request, Response } from "express";
 
@@ -36,18 +38,25 @@ export const getShareableLink = async (req: Request, res: Response) => {
     try {
         //@ts-ignore
         const userId = req.userId;
+        const hash = req.params.hash;
 
         if (!userId) {
             return res.status(401).json({ msg: "Unauthorized" });
         }
 
-        const link = await linkModel.findOne({ userId });
+        const link = await linkModel.findOne({ hash });
 
         if (!link) {
             return res.status(404).json({ msg: "No shareable link found" });
         }
+        const content=await contentModel.findOne({userId:link.userId});
+        const user=await userModel.findById(link.userId);
+        if(!content||!user){
+            return res.status(404).json({ msg: "No content found for this link" });
+        }
 
-        return res.status(200).json({ hash: link.hash });
+
+        return res.status(200).json({ msg: "Shareable link fetched successfully", user:{name:user.username}, content });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ msg: "Server Error", error });
